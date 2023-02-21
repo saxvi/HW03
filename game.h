459d2922a0789d9dfe7a1dbb327fca176d93a68f
sadie m <smothershed3@gatech.edu>
2023-02-21 18:53:28 -0500

    added obst and player structs

--- game.h
+++ game.h
@@ -6,8 +6,20 @@ typedef struct {
     int y;
     int oldx;
     int oldy;
-    int xVelocity;
+    int xvel;
+    int yvel;
     int width;
     int height;
     unsigned short color;
 } PLAYER;
+
+//obstacle struct
+typedef struct {
+    int x;
+    int y;
+    int oldx;
+    int oldy;
+    int yvel;
+    int width;
+    int height;
+} OBST;
