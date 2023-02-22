df200edbcaa67aa5a41104fffe8f8fa934479457
sadie m <smothershed3@gatech.edu>
2023-02-21 22:35:14 -0500

    added sound file

--- game.c
+++ game.c
@@ -1,6 +1,13 @@
 #include "game.h"
 #include "gba.h"
-#include 
+#include "sound.h"
 
+// structs on da screen
 PLAYER player;
 OBST obstacle;
+
+// initialize game
+void initGame() {
+    initPlayer();
+    initObsts();
+}
